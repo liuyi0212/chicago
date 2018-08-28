2cbae19173bccd1cf8ef45450ba42d97fcf01e47
liuy <liuy@liuydeMacBook-Pro.local>
2018-08-28 17:51:56 +0800

    test

--- src/main/java/com/medishare/chicago/view/test.java
+++ src/main/java/com/medishare/chicago/view/test.java
@@ -4,7 +4,7 @@ package com.medishare.chicago.view;
  * @author: liuy
  * @date: 2018/8/28
  * @time: 下午5:47
- * @descripttion: com.medishare.chicago.view
+ * @descripttion: com.medishare.chicago.view12312
  */
 public class test {
 }
